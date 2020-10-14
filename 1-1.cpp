4e181d3f0e7032c9588cb2b4101e43e3cb92b94d
unknown <marrugo.v@gmail.com>
2020-10-14 18:48:14 -0500

    1-1

---
+++ 1-1.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  int ingresado = 0;
+  string resultado = "";
+  const int l_inferior = 100, l_superior = 200;
+  cout << "por favor ingrese un numero: ";
+  cin >> ingresado;
+  cout << "\n ============================= \n";
+  if (ingresado < 100 || ingresado > 200)
+    {
+      resultado = "Se encuentra";
+    }
+  else
+    {
+      resultado = "No se encuentra";
+    }
+  cout << "el numero ingresado " << resultado <<
+    " dentro de los limites predeterminados.";
+
+  return 0;
+}
