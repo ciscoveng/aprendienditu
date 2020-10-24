4987773f68e486b89fbef4cfa259530f3a3019e7
cisco <marrugo.v@gmail.com>
2020-10-24 04:20:40 -0500

    TERMINADO PRRO

--- Hangman.cpp
+++ Hangman.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-#include<ctime>
+#include <ctime>
+#include <cctype>
 
 void cls(){//esto es pa poder emular un cls, porque en esta pinshi versh nuncun comando para limpiar la pantalla sirvió//
        printf("\x1b[2J\n");
@@ -10,11 +11,13 @@ using namespace std;
 
 int main ()
 {
-    int categoria=0, random=0, intentos=0;
-    string palabra,jugador,letras;
+    int categoria=0, random=0, intentos=6, largo;
+    string palabra,jugador;
+    char ingreso;
     string frutas[3]={"manzana","kiwi","lulo"};
     string animales[3]={"leon","gato","ornitorrinco"};
     string instrumentos[3]={"guitarra","saxofon","violin"};
+    bool error;
   
   //inicio de la eleccion de categoria y generacion de la plabra, con bucle por si se selecciona una opcion incorrecta//
   do{
@@ -40,22 +43,44 @@ int main ()
         
         default:
         cls();
-                cout<<"NO INGRESO UNA OPCION CORRECTA. INTENTE DE NUEVO"<<"\n";
+                cout<<"\u001b[31mNO INGRESO UNA OPCION CORRECTA. INTENTE DE NUEVO\u001b[37m"<<"\n";
             break;
       }
   }while (categoria>3);// finaliza el menú y la generacion de la palabra//
+  
+  largo=palabra.length();//desde aquí, sse obtiene el largo de la palabra. se carga en el juego y se reemplaza por _
+  jugador=palabra;
+  for(int i=0; i<largo; i++){
+      jugador[i]='_';
+  }//aquí acaba la carga y reemplazo del juego
 
-  //comienzo del juego//BUSCAR LA MANERA DE OBTENER EL LARGO DE PALABRA Y REEMPLAZAR JUGADOR CON TANTOS _ como sea el largo de la plabra
+  //comienzo del juego
   do  {
-      //AQuÍ IRIA EL JUEGO..... SI TUVIERA UNO//
-  } while (intentos<10 && palabra!=jugador);
+      cls();
+      error=true;//fue laa unica manera que  encontré de poder colocar el contador de errores. taba dificil, we
+      cout<<jugador<<"\n";
+      cout<<"te quedan: "<<intentos<<" intentos\n";
+      cin>>ingreso;
+      ingreso= tolower(ingreso);
+      for(int i=0; i<largo; i++){
+          if(palabra[i]==ingreso){
+              jugador[i]=ingreso;
+              error=false;
+          }
+      }
+      if(error==true){
+          intentos--;
+      }
+
+  } while (intentos!=0 && palabra!=jugador);
+  cls();
   if (palabra==jugador)
   {
-      cout<<"========FELICIDADES, HAS GANADO!!==========="<<"\n";
+      cout<<"\u001b[32m========FELICIDADES, HAS GANADO!!==========="<<"\n";
   }else
   {
-      cout<<"========PERDISTE CACHON, COMO TODO EN TU VIDA========"<<"\n";
+      cout<<"\u001b[31m========PERDISTE CACHON, COMO TODO EN TU VIDA========"<<"\n";
   }
-  
+  cout<<"la palabra era: "<<palabra;  
     return 0;
 }//fin
